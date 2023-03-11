3d993089a9afd4762624837b1e0bed1e2b648385
leand <leandroyyasuda@gmail.com>
2023-03-10 21:23:05 -0300

    Integracao com Mysql

---
+++ 54_0801_0808/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,65 @@
+package br.com.erudio.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices service;
+	
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		
+		return this.service.findAll();
+	}
+	
+	@GetMapping(
+			value = "/{id}", 
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id")Long id) throws Exception  {
+
+		return this.service.findById(id);
+	}
+	
+	@PostMapping(
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person){
+
+		return this.service.create(person);
+	}
+	
+	@PutMapping(
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person){
+
+		return this.service.update(person);
+	}
+	
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id")Long id){
+		this.service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+}
